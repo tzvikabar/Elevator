1c25f0dfadd51a2c013ad30647275b3101ecaedf
tzvikabar <tzvibaruchman@gmail.com>
2024-05-20 11:53:17 +0300

    tsc

--- dist/Components/Elevator.js
+++ dist/Components/Elevator.js
@@ -41,8 +41,6 @@ export default class Elevator {
                 this.movingTime = movingTime;
                 this.floorDestinationNumber = floorNumber;
                 this.isAvailable = false;
-                const currentPosition = this.currentPosition;
-                const newPosition = Math.round(floorNumber * floorHeightConfig);
                 // find the element
                 const elevator = document.querySelector(`#elevator${this.elevatorNumber}[buildingNumberData="${this.building.buildingNumber}"]`);
                 if (elevator) {
